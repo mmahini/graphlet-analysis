a454572324482a8e1dba7114c2605157893a22a3
Salman <salmanqadami@gmail.com>
2023-03-05 02:16:22 +0330

    chore: update async guise calc

--- guise_error_calculation.py
+++ guise_error_calculation.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 
 import os
+import ast
 from graph.graph import Graph, GraphFactory
 from algorithms.guise import Guise
 from algorithms.gfd import GfdUtils, EpsilonDelta
 from algorithms.exact import Exact
 from multiprocessing import Pool, Process
 
-num_of_running_alg = 20
+num_of_running_alg = 4
 files_path = './files'
 
 
-def file_uri(i=0) -> str:
-    return f'{files_path}/{i}.txt'
+def file_uri_gfd(i=0) -> str:
+    return f'{files_path}/gfd_{i}.txt'
 
+
+def file_uri_vertex_gfd(i=0) -> str:
+    return f'{files_path}/vertex_gfd_{i}.txt'
+
+
+def file_uri_gdv_vertex_orbits(i=0) -> str:
+    return f'{files_path}/gdv_vertex_orbits_{i}.txt'
 
 # initialize error calculation files
 # delete existed files from last calculation
@@ -34,10 +42,18 @@ def calc_guise_error(num_of_run=0, n=0, p=0.0):
     guise: Guise = Guise(g)
     guise.run(10000, 10000)
 
-    guise_error = GfdUtils().calc_graphlet_count_error(guise, exact)
+    error_gc = GfdUtils().calc_graphlet_count_error(guise, exact)
+    error_vgc = GfdUtils().calc_vertex_graphlet_count_error(guise, exact)
+    error_voc = GfdUtils().calc_vertex_orbit_count_error(guise, exact)
 
-    with open(file_uri(num_of_run), 'w') as fp:
-        fp.write(str(guise_error))
+    with open(file_uri_gfd(num_of_run), 'w') as fp:
+        fp.write(str(error_gc))
+
+    with open(file_uri_vertex_gfd(num_of_run), 'w') as fp:
+        fp.write(str(error_vgc))
+
+    with open(file_uri_gdv_vertex_orbits(num_of_run), 'w') as fp:
+        fp.write(str(error_voc))
 
 
 def do_calculation():
@@ -59,16 +75,35 @@ def do_calculation():
     for p in process_list:
         p.join()
 
-    epsilonDelta = EpsilonDelta()
+    graphlet_count_ed = EpsilonDelta([0.1, 0.05, 0.02, 0.01])
+    vertex_graphlet_count_ed = EpsilonDelta([0.05, 0.02, 0.01, 0.005])
+    vertex_orbit_count_ed = EpsilonDelta([0.01, 0.005, 0.002, 0.001])
+
     # read calculated errors from file
     for i in range(num_of_running_alg):
-        with open(file_uri(i), 'r') as fp:
-            giuse_error = fp.read()[1:-1]
-            giuse_error = [float(i) for i in giuse_error.split((', '))]
-            epsilonDelta.append_graphlet_count_error(giuse_error)
+        # read gfd errors
+        with open(file_uri_gfd(i), 'r') as fp:
+            giuse_error = ast.literal_eval(fp.read())
+            graphlet_count_ed.append_graphlet_count_error(giuse_error)
+        # read vertex gfd errors
+        with open(file_uri_vertex_gfd(i), 'r') as fp:
+            giuse_error = ast.literal_eval(fp.read())
+            vertex_graphlet_count_ed.append_vertex_count_error(giuse_error)
+        # read vertex gfd errors
+        with open(file_uri_gdv_vertex_orbits(i), 'r') as fp:
+            giuse_error = ast.literal_eval(fp.read())
+            vertex_orbit_count_ed.append_vertex_count_error(giuse_error)
 
-    epsilonDelta.calc_delta()
-    epsilonDelta.write()
+    graphlet_count_ed.calc_delta()
+    vertex_graphlet_count_ed.calc_delta()
+    vertex_orbit_count_ed.calc_delta()
+
+    print("\ngraphlet count")
+    graphlet_count_ed.write()
+    print("\nvertext graphlet count")
+    vertex_graphlet_count_ed.write()
+    print("\nvertext orbit count")
+    vertex_orbit_count_ed.write()
 
 
 if __name__ == '__main__':
