07ff0c4492d4ca80464b3d01a51041121755b556
salman qadami <ghadami@viratech.ir>
2023-10-21 14:31:35 +0330

    change exact test case

--- test/exact.py
+++ test/exact.py
@@ -2,14 +2,24 @@
 import sys, os
 sys.path.insert(1, os.getcwd())
 
+import time
 from graph.graph import Graph, GraphFactory
 from algorithms.exact import Exact
 
 # Test exact graphlet count calculation based on random generated graph
 if __name__ == "__main__":
-    g: Graph = GraphFactory().load_graph_from_cmd()
-    print(g)
+    n = int(input())
+    p = float(input())
 
-    exact: Exact = Exact(g)
-    exact.log = True
-    exact.run()
+    for i in range(10):
+        print(f"run {i} ...")
+
+        g: Graph = GraphFactory().get_random_instance(n, p)
+
+        print("calc exact")
+        start = time.time()
+        exact: Exact = Exact(g)
+        exact.run()
+        print(f"calc exact at {time.time() - start} seconds")
+
+    print(f"\ndone!!!")
