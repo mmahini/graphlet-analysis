c2cf7de324fd61bd741c6abcaf1835d86a16e531
salman qadami <ghadami@viratech.ir>
2023-10-22 19:14:10 +0330

    update exact test

--- test/exact.py
+++ test/exact.py
@@ -22,4 +22,15 @@ if __name__ == "__main__":
         exact.run()
         print(f"calc exact at {time.time() - start} seconds")
 
+        exact_graphlet_count = sum(len(v) for v in exact.gs.graphlets.values())
+        if exact_graphlet_count != exact.gs.total_num_of_graphlets:
+            print(f"Error, exact_graphlet_count: {exact_graphlet_count}, total_num_of_graphlets: {exact.gs.total_num_of_graphlets}")
+
+        for i in range(len(exact.gs.graphlet_cnt)):
+            j1 = exact.gs.graphlet_cnt[i]
+            j2 = len(exact.gs.graphlets[i])
+            if j1 != j2:
+                print(f"prev exact: {i}= {j1}")
+                print(f"new exact: {i}= {j2}")
+                
     print(f"\ndone!!!")
