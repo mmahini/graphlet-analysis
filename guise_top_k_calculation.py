1b5d0d94f741b3cf4667684da81b15eee167163d
salman qadami <ghadami@viratech.ir>
2023-11-03 14:27:27 +0330

    add top k guise calc

--- guise_top_k_calculation.py
+++ guise_top_k_calculation.py
@@ -1,125 +1,85 @@
 #!/usr/bin/env python3
+import sys, os
+sys.path.insert(1, os.getcwd())
 
-import os
-import ast
-from graph.graph import Graph, GraphFactory
-from algorithms.guise import Guise
-from algorithms.gfd import GfdUtils, EpsilonDelta
 from algorithms.exact import Exact
-from multiprocessing import Pool, Process
+from algorithms.gfd import EpsilonDelta, GfdUtils
+from algorithms.guise import Guise
+from graph.graphlet import NUM_OF_GRAPHLETS
+from graph.graph import Graph, GraphFactory
+from concurrent.futures import ThreadPoolExecutor
+import time
+
 
 num_of_running_alg = 10
-files_path = './t-files'
-stationary_steps = 10000
-n_steps = 10000
+stationary_steps = 1000
+n_steps = 200
+m_steps = 1000
+k = 5
+epsilon = 0.0075
 
+## Test guise graphlet count diff with exact count
+def guise_error_calculation():
+    n = int(input())
+    p = float(input())
+    
+    guise_correctness_list = list()
+    for i in range(num_of_running_alg):
+        print(f"run {i} ...")
+        guise_correctness = t(n, p)
+        if guise_correctness < 0:
+            print(f"error!!!, p is smaller than zero")
+            continue
+        guise_correctness_list.append(guise_correctness)
+ 
+    
+    print(f"guise_correctness_average: {sum(guise_correctness_list) / len(guise_correctness_list) * 100}")
 
-def file_uri_gfd(i=0) -> str:
-    return f'{files_path}/gfd_{i}.txt'
-
-
-def file_uri_gcd(i=0) -> str:
-    return f'{files_path}/gcd_{i}.txt'
-
-# initialize error calculation files
-# delete existed files from last calculation
-
-
-def init_files():
-    if os.path.isdir(files_path):
-        os.system('rm -rf %s' % files_path)
-    os.mkdir(files_path)
-
-
-def calc_guise_error(num_of_run=0, n=0, p=0.0):
+def t(n: int, p: float) -> float:
     g: Graph = GraphFactory().get_random_instance(n, p)
-    # print(g)
-
+    
     exact: Exact = Exact(g)
     exact.run()
 
-    guise: Guise = Guise(g)
-    guise.run(stationary_steps, n_steps)
+    guise_with_n_steps: Guise = Guise(g)
+    guise_with_n_steps.run(stationary_steps, n_steps)
+    print(f"graphlet_freq={sorted(guise_with_n_steps.gs.graphlet_freq,reverse=True)}")
+        
+    p = sorted(guise_with_n_steps.gs.graphlet_freq,reverse=True)[k-1] - epsilon
+    print(f"p={p}")
+    print(f"p correctness:{p <= sorted(exact.gs.graphlet_freq, reverse=True)[k-1]}")
+    if p <= 0:
+        return -1
+        
+    m_steps = int(n_steps / (p * 2))
+    print(f"m_steps={m_steps}")
 
-    gfd_top_kـcorrectness = GfdUtils().calc_gfd_top_kـcorrectness(guise, exact)
-    gcd_top_kـcorrectness = GfdUtils().calc_gcd_top_kـcorrectness(guise, exact)
+    guise_with_m_steps: Guise = Guise(g)
+    guise_with_m_steps.run(stationary_steps, m_steps)
 
-    with open(file_uri_gfd(num_of_run), 'w') as fp:
-        fp.write(str(gfd_top_kـcorrectness))
+    p_prime = sorted(guise_with_m_steps.gs.graphlet_freq,reverse=True)[k - 1] / (1 + epsilon)
+    print(f"p_prime={p_prime}")
 
-    with open(file_uri_gcd(num_of_run), 'w') as fp:
-        fp.write(str(gcd_top_kـcorrectness))
+    t_g_k = list()
+    exact_top_graphlet_freq = dict()
 
+    for i in range(NUM_OF_GRAPHLETS):
+        exact_top_graphlet_freq[i] = exact.gs.graphlet_freq[i]
 
-def do_calculation():
-    num_of_graph_v = int(input())
-    num_of_graph_p = float(input())
+        if (guise_with_m_steps.gs.graphlet_freq[i] / (1 - epsilon) > p_prime):
+            t_g_k.append(i)
 
-    print("calculation started, please wait")
-
-    init_files()
-
-    # parallel calculation
-    process_list = list()
-    for i in range(num_of_running_alg):
-        p = Process(
-            target=calc_guise_error,
-            args=(i, num_of_graph_v, num_of_graph_p))
-        p.start()
-        process_list.append(p)
-    for p in process_list:
-        p.join()
-
-    gfd_top_kـcorrectness = list()
-    gcd_top_kـcorrectness = list()
-
-    # read calculated errors from file
-    for i in range(num_of_running_alg):
-        # read gfd errors
-        with open(file_uri_gfd(i), 'r') as fp:
-            top_kـcorrectness = ast.literal_eval(fp.read())
-            gfd_top_kـcorrectness.append(top_kـcorrectness)
-        # read vertex gfd errors
-        with open(file_uri_gcd(i), 'r') as fp:
-            top_kـcorrectness = ast.literal_eval(fp.read())
-            gcd_top_kـcorrectness.append(top_kـcorrectness)
-
-    gfd_top_5ـcorrectness = 0
-    for i in gfd_top_kـcorrectness:
-        gfd_top_5ـcorrectness += i[0]
-    print(
-        f"\ngfd top 5 correctness: {gfd_top_5ـcorrectness / len(gfd_top_kـcorrectness)}")
-
-    gfd_top_10ـcorrectness = 0
-    for i in gfd_top_kـcorrectness:
-        gfd_top_10ـcorrectness += i[1]
-    print(
-        f"\ngfd top 10 correctness: {gfd_top_10ـcorrectness / len(gfd_top_kـcorrectness)}")
-
-    gfd_top_20ـcorrectness = 0
-    for i in gfd_top_kـcorrectness:
-        gfd_top_20ـcorrectness += i[2]
-    print(
-        f"\ngfd top 20 correctness: {gfd_top_20ـcorrectness / len(gfd_top_kـcorrectness)}")
-
-    gcd_top_5ـcorrectness = 0
-    for i in gcd_top_kـcorrectness:
-        gcd_top_5ـcorrectness += i[0]
-    print(
-        f"\ngcd top 5 correctness: {gcd_top_5ـcorrectness / len(gcd_top_kـcorrectness)}")
-
-    gcd_top_10ـcorrectness = 0
-    for i in gcd_top_kـcorrectness:
-        gcd_top_10ـcorrectness += i[1]
-    print(
-        f"\ngcd top 10 correctness: {gcd_top_10ـcorrectness / len(gcd_top_kـcorrectness)}")
-
-    gcd_top_20ـcorrectness = 0
-    for i in gcd_top_kـcorrectness:
-        gcd_top_20ـcorrectness += i[2]
-    print(
-        f"\ngcd top 20 correctness: {gcd_top_20ـcorrectness / len(gcd_top_kـcorrectness)}")
-
-
-if __name__ == '__main__':
-    do_calculation()
+    exact_top_k = [key for key, v in sorted(exact_top_graphlet_freq.items(), reverse=True, key=lambda item: item[1])][0:k]
+        
+    guise_correctness = len(set(exact_top_k) & set(t_g_k)) / len(exact_top_k)
+    
+    print(f"t_g_k: {t_g_k}")
+    print(f"exact_top_k: {exact_top_k}")
+    print(f"guise_correctness: {guise_correctness}")
+    print("\n")
+    
+    return guise_correctness
+    
+    
+if __name__ == "__main__":
+    guise_error_calculation()
